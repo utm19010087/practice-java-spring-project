9262b9999cb0895a40c16c39884d12a311b1f461
utm19010087 <utm19010087@utma.edu.mx>
2023-11-15 12:30:44 -0600

    modulo eliminacion de resultados

---
+++ src/main/java/org/utma/ItepTest/controller/editor/UsuarioEmailPropertyEditor.java
@@ -0,0 +1,14 @@
+package org.utma.ItepTest.controller.editor;
+
+import org.springframework.stereotype.Component;
+
+import java.beans.PropertyEditorSupport;
+
+@Component
+public class UsuarioEmailPropertyEditor extends PropertyEditorSupport
+{
+    @Override
+    public void setAsText(String text) throws IllegalArgumentException {
+        setValue(text.toLowerCase().trim());
+    }
+}
---
+++ src/main/java/org/utma/ItepTest/controller/error/ErrorHandlerController.java
@@ -0,0 +1,22 @@
+package org.utma.ItepTest.controller.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class ErrorHandlerController
+{
+    @ExceptionHandler({Exception.class})
+    public String excepcionGenerica(Exception ex, Model model)
+    {
+        model.addAttribute("error", "Error inesperado!");
+        model.addAttribute("message",ex.getMessage());
+        model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        model.addAttribute("timestamp", new Date());
+        return "error/generico";
+    }
+}
